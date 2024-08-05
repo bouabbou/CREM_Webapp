212f145dc1f78f5bab176e569bb05d0a04497c7a
noyl <nouhaylaennadri@gmail.com>
2024-08-05 10:49:38 +0200

    Refactor: Modularized header and footer into reusable components, implemented dynamic logo handling using context variables, and updated templates to use includes for improved maintainability and organization.

--- front_app/views.py
+++ front_app/views.py
@@ -1,4 +1,5 @@
 # CREM/views.py
+from django.templatetags.static import static
 
 from django.shortcuts import render
 
@@ -13,4 +14,6 @@ def innovation(request):
 def services(request):
     return render(request, 'Services_FA_EN.html')
 def index(request):
+    return render(request, 'index_FA_EN.html')
+def presentation(request):
     return render(request, 'index_FA_EN.html')
