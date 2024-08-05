fda7b49341112df2bdee11b895317e9ae3686546
noyl <nouhaylaennadri@gmail.com>
2024-08-05 18:16:17 +0200

    Switched database from MySQL to SQLite

--- CREM_Webapp/settings.py
+++ CREM_Webapp/settings.py
@@ -74,12 +74,8 @@ WSGI_APPLICATION = "CREM_Webapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "infrustructure",
-        "USER": "root",
-        "PASSWORD": "2002",
-        "HOST": "localhost",
-        "PORT": "3306", 
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
