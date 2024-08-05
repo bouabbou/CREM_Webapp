65318cf610b69aa40500796df6031cd52e08c581
noyl <nouhaylaennadri@gmail.com>
2024-08-05 14:35:37 +0200

    Add requirements.txt and configure SQL database

--- CREM_Webapp/settings.py
+++ CREM_Webapp/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "front_app",  # Your app
+    "front_app",  
+    "instrumentation",
 ]
 
 MIDDLEWARE = [
@@ -73,8 +74,12 @@ WSGI_APPLICATION = "CREM_Webapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "infrustructure",
+        "USER": "root",
+        "PASSWORD": "2002",
+        "HOST": "localhost",
+        "PORT": "3306", 
     }
 }
 
