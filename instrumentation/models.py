6846fc90c64eb8e699f7af433d7bbdf2618eb744
noyl <nouhaylaennadri@gmail.com>
2024-08-15 16:27:29 +0200

    gitigone

--- instrumentation/models.py
+++ instrumentation/models.py
@@ -3,6 +3,7 @@ import os
 
 class Platform(models.Model):
     name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=10, default="N/A")
 
     def __str__(self):
         return self.name
@@ -25,14 +26,11 @@ class Product(models.Model):
         return self.name
 
     def get_image_upload_path(self, filename):
-        # Get the platform name from the category's platform
+        
         platform_name = self.category.platform.name
-        # Define the directory structure
         folder_name = f'infrastructure/{platform_name}/'
-        # Join the folder name with the filename
         return os.path.join(folder_name, filename)
 
     def save(self, *args, **kwargs):
-        # Set the upload_to path dynamically based on the platform
         self.image.field.upload_to = self.get_image_upload_path(self.image.name)
         super(Product, self).save(*args, **kwargs)
