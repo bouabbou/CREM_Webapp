f576eb0d4f3543be059ba0c7d099b575bdf433c8
bouabbou <bouabbou@gmail.com>
2024-09-17 12:09:49 +0200

    settings

--- CREM_Webapp/settings.py
+++ CREM_Webapp/settings.py
@@ -33,7 +33,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'elhammamimeryem@gmail.com'
 EMAIL_HOST_PASSWORD = 'xkgb syxd bsmv qxel'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.16.80.130']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
