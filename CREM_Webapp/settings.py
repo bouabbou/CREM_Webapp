fb80dfe35803d3c2d2082d44490fa2f6d9c1172b
bouabbou <bouabbou@gmail.com>
2025-04-22 10:26:44 +0100

    fixing logs

--- CREM_Webapp/settings.py
+++ CREM_Webapp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # Add your static files directory here
+# STATICFILES_DIRS = [BASE_DIR / "static"]  # Add your static files directory here
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
